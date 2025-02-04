3ead0a163d8ca3ed84ff988b4d164ff32abe6ecc
je0ng3 <jepark2934@gmail.com>
2025-02-05 02:21:56 +0900

    [Refactor] refactor plain Java code to use JPA

---
+++ src/main/java/com/capstone/favicon/domain/domain/User.java
@@ -0,0 +1,22 @@
+package com.capstone.favicon.domain;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    private String username;
+    private String password;
+
+}
