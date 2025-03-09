771e505c4270576e473f5af7d1c1be4bfaac37c6
sungwoo8763 <161145164+sungwoo8763@users.noreply.github.com>
2025-03-09 10:21:31 +0900

    Revert "메인 및 데이터 목록 페이지 / 상세정보"

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM gradle:8.4-jdk17 AS builder
-WORKDIR /app
-COPY . .
-RUN ./gradlew bootJar
-
-FROM amazoncorretto:17.0.12
-WORKDIR /app
-COPY --from=builder /app/build/libs/*.jar app.jar
-ENTRYPOINT ["java", "-jar", "-Djava.security.egd=file:/dev/./urandom", "app.jar"]
---
+++ src/main/java/com/capstone/favicon/user/dto/RegisterDto.java
@@ -0,0 +1,25 @@
+package com.capstone.favicon.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterDto {
+
+    private String email;
+    private String username;
+    private String password;
+
+    @Getter
+    public static class checkEmail {
+        private String email;
+    }
+
+    @Getter
+    public static class checkCode {
+        private String email;
+        private String code;
+    }
+
+}
