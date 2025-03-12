c83748d6a78ae449e712dfc573eae46b21e4e043
oneyVerde <jhoney384@naver.com>
2025-03-12 15:05:16 +0900

    [Refactor] delete unused import

--- src/main/java/com/capstone/favicon/config/Utf8Filter.java
+++ src/main/java/com/capstone/favicon/config/Utf8Filter.java
@@ -1,6 +1,5 @@
 package com.capstone.favicon.config;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import jakarta.servlet.*;
