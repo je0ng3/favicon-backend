e882c8f5f641f8b4b7dde2e122419dd4b8cdc2c9
sungwoo8763 <sungwoo8763@gmail.com>
2025-03-16 11:55:58 +0900

    data and theme api edit

--- src/main/java/com/capstone/favicon/dataset/controller/DatasetController.java
+++ src/main/java/com/capstone/favicon/dataset/controller/DatasetController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/dataset")
+@RequestMapping("/data-set")
 public class DatasetController {
 
     private final DatasetService datasetService;
