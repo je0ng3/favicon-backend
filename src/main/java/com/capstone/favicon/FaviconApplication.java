5ff2faff5569218a4d947f688ef791894180b1fd
je0ng3 <jepark2934@gmail.com>
2025-09-20 15:12:27 +0900

    refactor: skip AWS/GPT bean registration at runtime

--- src/main/java/com/capstone/favicon/FaviconApplication.java
+++ src/main/java/com/capstone/favicon/FaviconApplication.java
@@ -2,9 +2,18 @@ package com.capstone.favicon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(
+                type = FilterType.REGEX,
+                pattern = {"com\\.capstone\\.favicon\\.aws\\..*",
+                            "com\\.capstone\\.favicon\\.dataset\\.controller\\.GPTController"}
+        )
+)
 @EnableScheduling
 public class FaviconApplication {
 
