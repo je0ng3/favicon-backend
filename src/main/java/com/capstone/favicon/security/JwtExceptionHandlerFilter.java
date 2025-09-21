294a8f35253797f78e09247238b6ce38b66f8db6
je0ng3 <jepark2934@gmail.com>
2025-09-21 14:42:58 +0900

    fix: add missing annotation

--- src/main/java/com/capstone/favicon/security/JwtExceptionHandlerFilter.java
+++ src/main/java/com/capstone/favicon/security/JwtExceptionHandlerFilter.java
@@ -5,10 +5,12 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 public class JwtExceptionHandlerFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request,
